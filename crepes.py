5d37a73e65ba1bb83bb2179ff0e4f43051122eec
Mergify <37929162+mergify[bot]@users.noreply.github.com>
2022-08-02 11:52:01 +0000

    Restyle Create crepes.py (#3) Restyled by autopep8
    * Restyled by black

--- crepes.py
+++ crepes.py
@@ -1,1 +1,3 @@
-print ('You there! Eat one of those mushrooms, and then, after a quick disassembly, I will find out what it did to you!')
+print(
+    "You there! Eat one of those mushrooms, and then, after a quick disassembly, I will find out what it did to you!"
+)
